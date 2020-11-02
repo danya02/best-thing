c53adb30ad36c1c47bd53c53e0716ea0cfade7e7
Danya Generalov <danya@danya02.ru>
2020-11-02 08:53:53 +0300

    Adding necessary tables.

--- database.py
+++ database.py
@@ -1,8 +1,23 @@
 from peewee import *
 
 db = MySQLDatabase('best', user='best', password='best', host='10.0.0.2')
-db.connect()
 
 class MyModel(Model):
     class Meta:
         database = db
+
+def create_table(table):
+    db.create_tables([table])
+    return table
+
+@create_table
+class Thing(MyModel):
+    title = CharField(unique=True)
+    description = TextField()
+    image = CharField(null=True) # URL to image
+
+@create_table
+class VoteCount(MyModel):
+    thing = ForeignKeyField(Thing, primary_key=True, unique=True)
+    matchups = IntegerField(index=True)
+    wins = IntegerField(index=True)
